0fd5f342af1d79b47d7957b4f3977918a82dd0a3
IKovalevsky <ivan.kavalski927@gmail.com>
2024-01-07 00:38:11 +0300

    model is written

--- TO_DO_List/app/models.py
+++ TO_DO_List/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE, null= True, blank= True)
+    title = models.CharField(max_length= 15)
+    description = models.TextField(null= True, blank=True)
+    complete = models.BooleanField(default= False)
+    time_created = models.DateTimeField(auto_now_add= True)
+    file_task = models.FileField(upload_to="uploads/%Y/%m/%d/")
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        article_belongs_to = 'user'
